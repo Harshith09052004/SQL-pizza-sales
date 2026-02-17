c40e275f94040902d85b362e4dd39f22b53be525
Harshith09052004 <podduturiharshith47@gmail.com>
2026-02-17 10:46:11 +0530

    Create pizza_sales table in database

---
+++ database.sql
@@ -0,0 +1,9 @@
+CREATE TABLE pizza_sales (
+    order_id INT,
+    pizza_name VARCHAR(100),
+    category VARCHAR(50),
+    size VARCHAR(10),
+    quantity INT,
+    price DECIMAL(5,2),
+    order_date DATE
+);
